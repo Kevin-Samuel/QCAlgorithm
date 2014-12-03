ffef29cd4cd2d7d6ab7befb03d371fdd072a8900
Jared Broad <jared@quantconnect.com>
2014-12-03 09:50:45 -0300

    Update

---
+++ QuantConnect.Algorithm/9_Algorithm_Test_OnEndOfDay.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace QuantConnect
+{
+    using QuantConnect.Securities;
+    using QuantConnect.Models;
+
+    public partial class TestOnEndOfDay : QCAlgorithm, IAlgorithm
+    {
+        string symbol = "SPY";
+
+        public override void Initialize()
+        {
+            SetStartDate(2013, 1, 1);
+            SetEndDate(2014, 1, 1);
+            SetCash(30000);
+            AddSecurity(SecurityType.Equity, symbol, Resolution.Minute);
+        }
+
+        public override void OnTradeBar(Dictionary<string, TradeBar> data)
+        {
+            if (Portfolio.HoldStock == false)
+            {
+                Order(symbol, 50);
+            }
+        }
+
+        public override void OnEndOfDay()
+        {
+            Debug(Time.Date.ToShortDateString() + " EOD Message.");
+        }
+    }
+}
